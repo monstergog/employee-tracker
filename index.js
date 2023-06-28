9beb482879940c67f7ea600b70de8f6985ebd8fb
monstergog <amn002@ucsd.edu>
2023-06-28 05:41:57 -0700

    added table viewing functionality

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
-const questions = require('./lib/utils/questions.js')
-const dbQuery = require('./lib/utils/dbquery.js')
+const consoleTable = require('console.table');
+const questions = require('./lib/utils/questions.js');
 require('dotenv').config();
 
 // Connect to database
@@ -15,16 +15,38 @@ const db = mysql.createConnection(
   console.log(`Connected to the registrar_db database.`)
 );
 
+function viewTable(table) {
+  db.query(`SELECT * FROM ${table}`, function (err, results) {
+    console.log();
+    console.table(results);
+    init();
+    });
+}
+
 // Function to prompt user for input and sent to be written
 async function init() {
-  for (let i = 0; i < 3; i++) {
-    await inquirer.prompt(questions)
-    .then(((data) => {
-      db.query('SELECT * FROM employees', function (err, results) {
-        console.log(results);
-      });
-    }))
-  }
+  await inquirer.prompt(questions)
+  .then(((data) => {
+    switch(data.options) {
+      case 'View All Employees':
+        viewTable('employees');
+        break;
+      case 'Add Employee':
+        break;
+      case 'Update Employee Role':
+        break;
+      case 'View All Roles':
+        viewTable('roles');
+        break;
+      case 'Add Role':
+        break;
+      case 'View All Departments':
+        viewTable('departments');
+        break;
+      case 'Add Department':
+        break;
+    }
+  }))
 };
 
 init();
