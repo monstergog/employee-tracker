28d7c9a42cdbbf9788b1434b83923547cd0625fd
monstergog <amn002@ucsd.edu>
2023-09-05 05:59:12 -0700

    moved mysql connection code to config folder + managing database more reader friendly

--- index.js
+++ index.js
@@ -1,24 +1,55 @@
-const mysql = require('mysql2');
+const db = require('./lib/config/connection.js')
 const inquirer = require('inquirer');
-const {questions, newEmployee, newRole, newDepartment, updateEmployee} = require('./lib/utils/questions.js');
-require('dotenv').config();
+const {questions, updateEmployee} = require('./lib/utils/questions.js');
 require('console.table');
 
-// Connect to database
-const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    user: process.env.dbusername,
-    password: process.env.dbpassword,
-    database: process.env.database
-  },
-  console.log(`Connected to the registrar_db database.`)
-);
-
-function viewTable(table) {
-  db.query(`SELECT * FROM ${table}`, (err, results) => {
+function viewAllEmployees() {
+  db.query(`SELECT
+  e1.id,
+  e1.first_name AS first_name,
+  e1.last_name AS last_name,
+  r.title,
+  d.name AS department,
+  r.salary,
+  CONCAT(e2.first_name, ' ', e2.last_name) AS manager
+  FROM employees AS e1
+  INNER JOIN roles AS r ON e1.role_id = r.id
+  LEFT JOIN departments AS d ON r.department_id = d.id
+  LEFT JOIN employees AS e2 ON e1.manager_id = e2.id;`,
+   (err, results) => {
     if (err) {
-      console.error('Error adding employee:', err);
+      console.error('Error finding employees:', err);
+      return;
+    }
+    console.log();
+    console.table(results);
+    init();
+    });
+}
+
+function viewAllRoles() {
+  db.query(`SELECT
+  r.id,
+  r.title,
+  d.name AS department,
+  r.salary
+  FROM roles AS r
+  INNER JOIN departments AS d ON r.department_id = d.id`,
+  (err, results) => {
+    if (err) {
+      console.error('Error finding roles:', err);
+      return;
+    }
+    console.log();
+    console.table(results);
+    init();
+    });
+}
+
+function viewAllDepartments() {
+  db.query(`SELECT * FROM departments`, (err, results) => {
+    if (err) {
+      console.error('Error finding departments:', err);
       return;
     }
     console.log();
@@ -33,10 +64,10 @@ function addDepartment(data) {
   [data.name],
   (err, results) => {
     if (err) {
-      console.error('Error adding employee:', err);
+      console.error('Error adding department:', err);
       return;
     }
-    console.log('New Department Added')
+    console.log(`New Department "${data.name}" Added`)
     init();
   });
 }
@@ -47,27 +78,25 @@ function addRole(data) {
   [data.title, data.salary, data.departmentID],
   (err, results) => {
     if (err) {
-      console.error('Error adding employee:', err);
+      console.error('Error adding role:', err);
       return;
     }
-    console.log('New Role Added')
+    console.log(`New Role "${data.title}" Added`)
     init()
   });
 }
 
 function addEmployee(data) {
-  const managerID = data.managerID.trim() === '' ? null : data.managerID;
-
   db.query(
     `INSERT INTO employees (first_name, last_name, role_id, manager_id)
     VALUES (?, ?, ?, ?)`,
-    [data.firstName, data.lastName, data.roleID, managerID],
+    [data.firstName, data.lastName, data.roleID, data.managerID],
     (err, results) => {
       if (err) {
         console.error('Error adding employee:', err);
         return;
       }
-      console.log('New Employee Added');
+      console.log(`New Role "${data.firstName + " " + data.lastName}" Added`);
       init();
     }
   );
@@ -89,28 +118,136 @@ function updateEmployeeData(data) {
   );
 }
 
+function getDepartments() {
+  return new Promise((resolve, reject) => {
+    db.query(`SELECT id, name FROM departments`, (err, results) => {
+      if (err) {
+        console.error(`Error finding departments`);
+        reject(err);
+      } else {
+        const columnData = results.map(({id, name}) => ({
+          name: name,
+          value: id
+        }));
+        resolve(columnData);
+      }
+    });
+  });
+}
+
+function getRoles() {
+  return new Promise((resolve, reject) => {
+    db.query(`SELECT id, title FROM roles`, (err, results) => {
+      if (err) {
+        console.error(`Error finding departments`);
+        reject(err);
+      } else {
+        const columnData = results.map(({id, title}) => ({
+          name: title,
+          value: id
+        }));
+        resolve(columnData);
+      }
+    });
+  });
+}
+
+function getManagers() {
+  return new Promise((resolve, reject) => {
+    db.query(`SELECT id, first_name, last_name FROM employees`, (err, results) => {
+      if (err) {
+        console.error(`Error finding departments`);
+        reject(err);
+      } else {
+        const columnData = results.map(({id, first_name, last_name}) => ({
+          name: first_name + ' ' + last_name,
+          value: id
+        }));
+        resolve(columnData);
+      }
+    });
+  });
+}
+
 // Function to prompt user for input and sent to be written
 async function init() {
   await inquirer.prompt(questions)
   .then(((data) => {
     switch(data.options) {
       case 'View All Employees':
-        viewTable('employees');
+        viewAllEmployees();
         break;
       case 'Add Employee':
-        inquirer.prompt(newEmployee).then((employeeData) => addEmployee(employeeData));
+        (async () => {
+          try {
+            let roles = await getRoles();
+            let managers = await getManagers();
+            managers.unshift({ name: 'None', value: null})
+            inquirer.prompt([
+              {
+                  type: 'input',
+                  name: 'firstName',
+                  message: 'What is FIRST name of the new Employee:'
+              },
+              {
+                  type: 'input',
+                  name: 'lastName',
+                  message: 'What is LAST name of the new employee:'
+              },
+              {
+                  type: 'list',
+                  name: 'roleID',
+                  message: 'What is role of the new employee:',
+                  choices: roles
+              },
+              {
+                  type: 'list',
+                  name: 'managerID',
+                  message: 'Who is the manager of this new employee:',
+                  choices: managers
+              }
+          ]).then((employeeData) => addEmployee(employeeData));
+          }
+          catch (error) {
+            console.error(error);
+          }
+        })();
         break;
       case 'Update Employee Role':
         inquirer.prompt(updateEmployee).then((updateData) => updateEmployeeData(updateData));
         break;
       case 'View All Roles':
-        viewTable('roles');
+        viewAllRoles();
         break;
-      case 'Add Role':
-        inquirer.prompt(newRole).then((roleData) => addRole(roleData));
-        break;
+        case 'Add Role':
+          (async () => {
+            try {
+              let departments = await getDepartments('name', 'departments');
+              inquirer.prompt([
+                {
+                  type: 'input',
+                  name: 'title',
+                  message: 'What is the title of the new role:'
+                },
+                {
+                  type: 'input',
+                  name: 'salary',
+                  message: 'What is the salary of the new role:'
+                },
+                {
+                  type: 'list',
+                  name: 'departmentID',
+                  message: 'What is the department of the new role:',
+                  choices: departments
+                }
+              ]).then((roleData) => addRole(roleData));
+            } catch (error) {
+              console.error(error);
+            }
+          })();
+          break;
       case 'View All Departments':
-        viewTable('departments');
+        viewAllDepartments();
         break;
       case 'Add Department':
         inquirer.prompt(newDepartment).then((departmentData) => addDepartment(departmentData));
